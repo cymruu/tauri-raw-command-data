c69263f7fc255d055d5654d554a5b82ed007ca4a
Filip Bachul <filipbachul@gmail.com>
2022-12-10 12:42:18 +0100

    add extra buttons

--- src/main.js
+++ src/main.js
@@ -17,17 +17,24 @@ window.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("#greet-button")
     .addEventListener("click", () => greet());
+
   document
     .querySelector("#action-spawn")
     .addEventListener("click", () => spawn())
+  document
+    .querySelector("#action-spawn-raw")
+    .addEventListener("click", () => spawn_raw())
 
   document
     .querySelector("#action-execute")
     .addEventListener("click", () => execute())
+  document
+    .querySelector("#action-execute-raw")
+    .addEventListener("click", () => execute_raw())
 });
 
 
-async function spawn() {
+async function spawn_raw() {
   const command = new TAURI.shell.Command('ipconfig', [], { encoding: 'raw' });
   command.on('close', data => {
     console.log(`command finished with code ${data.code} and signal ${data.signal}`)
@@ -39,9 +46,26 @@ async function spawn() {
   console.log('pid:', child.pid);
 }
 
+async function spawn() {
+  const command = new TAURI.shell.Command('ipconfig', []);
+  command.on('close', data => {
+    console.log(`command finished with code ${data.code} and signal ${data.signal}`)
+  });
+  command.on('error', error => console.error(`command error: "${error}"`));
+  command.stdout.on('data', line => console.log(`command stdout: "${line}"`, line));
+  command.stderr.on('data', line => console.log(`command stderr: "${line}"`, line));
+  const child = await command.spawn();
+  console.log('pid:', child.pid);
+}
+
+async function execute_raw() {
+  const command = new TAURI.shell.Command('ipconfig', [], { encoding: 'raw' });
+  const result = await command.execute();
+  console.log({ result });
+}
 
 async function execute() {
-  const command = new TAURI.shell.Command('ipconfig', [], { encoding: 'raw' });
+  const command = new TAURI.shell.Command('ipconfig', []);
   const result = await command.execute();
   console.log({ result });
 }
